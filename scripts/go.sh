faa7725b6b029bb22ff6084ea10f9177971052f7
aspenne <axel.mch@outlook.fr>
2023-03-13 19:41:51 +0100

    html to pdf is done but the pdf is unreadable

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,11 +2,13 @@
 
 cp -r ../modele/ /Docker/aspen/
 cp -r ../scripts/ /Docker/aspen/
+cp -r ../samples/multi.json /Docker/aspen/scripts
+cp -r ../samples/mono.json /Docker/aspen/scripts
 
-docker pull bigpapoo/sae4-html2pdf
-docker pull bigpapoo/sae4-php
+docker image pull bigpapoo/sae4-html2pdf
+docker image pull bigpapoo/sae4-php  
 
+docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-php php scripts/builder.php > /Docker/aspen/multi.html
+docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-html2pdf "html2pdf multi.html multi.pdf"
 
-docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-html2pdf "html2pdf modele/modele.html modele/modele.pdf"
-
-mv /Docker/aspen/modele/modele.pdf ../modele/
+mv /Docker/aspen/multi.html ../samples/multi.pdf
