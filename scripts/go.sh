a3da4c51ca3505a0d7ea7f9e5d93aefd9fb80a21
aspenne <axel.mch@outlook.fr>
2023-04-03 16:44:57 +0200

    remove old file at the end + dissociate multi and mono files

--- scripts/go.sh
+++ scripts/go.sh
@@ -8,10 +8,35 @@ cp -r ../samples/mono.json /Docker/aspen/scripts
 docker image pull bigpapoo/sae4-html2pdf
 docker image pull bigpapoo/sae4-php  
 
-docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-php php scripts/builder.php > /Docker/aspen/multi.html
+# docker build -t alizon-website ../../
+# docker build -t catalogueur ../
+
+# docker network create alizon-network
+
+# docker run -d --rm --name alizon-container --network alizon-network alizon-website
+# docker run -d --rm --name catalogueur-container  --network alizon-network catalogueur
+
+# docker container ls -a
+# docker network inspect alizon-network 
+# docker container ls -a
 
 mv /Docker/aspen/scripts/builder.css /Docker/aspen/builder.css
 
-docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-html2pdf "html2pdf multi.html multi.pdf"
+if [ -f /Docker/aspen/scripts/multi.json ]; then
+    docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-php php scripts/builder.php > /Docker/aspen/multi.html
+    docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-html2pdf "html2pdf multi.html multi.pdf"
+    mv /Docker/aspen/multi.pdf ../samples/multi.pdf
+else
+    docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-php php scripts/builder.php > /Docker/aspen/mono.html
+    docker container run --rm -ti -v /Docker/aspen:/work bigpapoo/sae4-html2pdf "html2pdf mono.html mono.pdf"
+    mv /Docker/aspen/mono.pdf ../samples/mono.pdf
+fi
 
-mv /Docker/aspen/multi.pdf ../samples/multi.pdf
+rm -r /Docker/aspen/scripts
+rm -r /Docker/aspen/modele
+rm  /Docker/aspen/builder.css
+rm /Docker/aspen/multi.html
+rm /Docker/aspen/mono.html
+
+# docker container rm alizon-website
+# docker container rm catalogueur
